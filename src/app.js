43cb23f155e582f2f83ab6119fdfaae84df4b099
alalalexexex <alextrainham@gmail.com>
2019-08-19 11:16:24 -0400

    Updates for front end

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 const express = require('express'); 
 const path = require('path'); 
+const hbs = require('hbs'); 
 
 // remember that nodejs projects are wrapped in a function
+// right now the hbs files have to be in the root of the project, and have to be stored in the directory called 'views'
 
+// Get absolute paths
+const partialsPath = path.join(__dirname, '../templates/partials');  
+const viewsPath = path.join(__dirname, '../templates/views'); 
+
+/*************************
+ * Setup handlebars config for express to find
+ * 'view engine' -> set to handlebars
+ * 'views' -> customize the directory name to templates
+ */
 const app = express(); 
-// cover what this is doing in detail soon
-
 app.set('view engine', 'hbs'); 
+app.set('views', viewsPath); 
+hbs.registerPartials(partialsPath); 
+
+// setupe path to static directory to serve. 
 app.use(express.static(path.join(__dirname, '../public'))); 
 
 app.get('', (req, res) => {
@@ -26,16 +39,44 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Need Help?',
+        name: 'Alex Trainham', 
         body: 'This is where you will get it :)'
     })
 })
 
 app.get('/weather', (req, res) => {
+    // send the json object to client
     res.send({
         location: 'Richmond', 
         forecast: 'The weather looking mad nice'
     }); 
 }); 
+
+app.get('/products', (req, res) => {
+    res.send({
+        products: [] 
+    })
+})
+
+// matches anything after /help/* and is more specific 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404 not found', 
+        errorMessage: 'Help page wasn\'t found', 
+        name: 'Alex Trainham'
+    })
+}); 
+
+// this specifically needs to be last because express 
+// looks the project linearly, from the top. 
+// wildcard matches anything. 
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404 Not found', 
+        errorMessage: 'Page wasn\'t found', 
+        name: 'Alex Trainham'
+    }); 
+})
 
 // app.com 
 // app.com/help
